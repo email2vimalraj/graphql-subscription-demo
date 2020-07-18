7267138ba9457b96e3f210c02215c58f31e9a4ba
Vimalraj Selvam <email2vimalraj@gmail.com>
2020-07-19 01:06:30 +0800

    Added apollo graphql client and completed the subscription with cancel feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,124 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Layout, Row, Col, Modal } from 'antd'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+  useQuery,
+  HttpLink,
+  split
+} from '@apollo/client'
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { getMainDefinition } from '@apollo/client/utilities'
+
+import './App.css'
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql'
+})
+
+const subscriptionLink = new WebSocketLink({
+  uri: 'ws://localhost:4000/graphql',
+  options: {
+    reconnect: true
+  }
+})
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    )
+  },
+  subscriptionLink,
+  httpLink
+)
+
+const client = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache()
+})
+
+const COUNT_QUERY = gql`
+  query CountQuery {
+    count
+  }
+`
+
+const COUNT_SUBSCRIPTION = gql`
+  subscription CountSubscription {
+    count
+  }
+`
+
+function Counter() {
+  const { loading, error, data, subscribeToMore } = useQuery(COUNT_QUERY)
+
+  const [modalVisible, setModalVisible] = React.useState(false)
+
+  React.useEffect(() => {
+    let unsubscribe
+
+    if (!modalVisible) {
+      unsubscribe = subscribeToMore({
+        document: COUNT_SUBSCRIPTION,
+        updateQuery: (prev, { subscriptionData }) => {
+          if (!subscriptionData.data) return prev
+
+          return {
+            count: subscriptionData.data.count
+          }
+        }
+      })
+    }
+
+    if (unsubscribe) return () => unsubscribe()
+  }, [modalVisible, subscribeToMore])
+
+  return (
+    <>
+      <h1
+        style={{ fontSize: 54, cursor: 'pointer' }}
+        onClick={() => setModalVisible(!modalVisible)}
+      >
+        {loading ? 'Loading' : error ? 'Error :(' : data.count}
+      </h1>
+      <Modal
+        title="Drill down"
+        visible={modalVisible}
+        onOk={() => setModalVisible(false)}
+        onCancel={() => setModalVisible(false)}
+      >
+        <p>Drill down here</p>
+      </Modal>
+    </>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ApolloProvider client={client}>
+      <Layout style={{ height: '100vh' }}>
+        <Layout.Content
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Row>
+            <Col span={24} style={{ textAlign: 'center', padding: '16px 0' }}>
+              <Counter />
+            </Col>
+          </Row>
+        </Layout.Content>
+      </Layout>
+    </ApolloProvider>
+  )
 }
 
-export default App;
+export default App
